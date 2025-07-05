25a79a33dd2a1c574afb40fd9d29a6ebc7434ab5
Sasha Isaienko <sashais1507@gmail.com>
2025-07-05 12:40:07 +0300

    commit message

---
+++ src/js/2-form.js
@@ -0,0 +1,51 @@
+const STORAGE_KEY = "feedback-form-state"
+
+const form = document.querySelector(".feedback-form");
+const textarea = form.querySelector("textarea");
+const input = form.querySelector("input");
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener("input", onTextareaInput);
+
+function onTextareaInput(event) {
+    const { name, value } = event.target;
+    formData[name] = value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+loadFormData();
+
+function loadFormData() {
+    const saveData = localStorage.getItem(STORAGE_KEY);
+
+    if (saveData) {
+        try {
+            formData = JSON.parse(saveData);
+
+            textarea.value = formData.message || '';
+            input.value = formData.email || '';
+        } catch (error) {
+
+        }
+    }
+}
+
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
